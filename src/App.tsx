c6156ea3a33a2c65acd6a372f044b3b042f6123f
FeBelluco <felipebelluco@live.com>
2022-06-06 03:02:29 -0300

    Segurança - Login - Cadastro Usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
 import "./App.css";
@@ -13,11 +14,10 @@ function App() {
       <div style={{ minHeight: "100vh" }}>
         <Routes>
           {" "}
-          // Antigo Switch
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
-  
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
